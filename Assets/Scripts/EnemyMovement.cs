1e6e2567c607915fa7670b406cdf0f603b627f3b
Marlen Xique <xiquemarlenv@gmail.com>
2022-05-12 16:04:50 -0400

    ahhh

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+
+    public float speed = 7.0f;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yValue = speed * Time.deltaTime;
+        transform.Translate(0, yValue, 0);
+
+    }
+}
